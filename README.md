53bf8473234942534aadc57263c340adfc53956d
Lockett Pundt <kalifornium@hotmail.com>
2020-09-28 08:35:54 -0400

    New project, who dis...

---
+++ README.md
@@ -0,0 +1,6 @@
+## Lockett's Create react App
+
+  Bootrapped with a very minimal and experimental version of this project. 
+  
+  To use eslint, type yarn eslintinit.
+  
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  import 'normalize.css';
+  
+  const Index = () => (
+    <h1>Hello World!</h1>
+  );
+  ReactDOM.render(<Index />, document.getElementById('root'));
+  
